12e44a32864c3dfc75259d7e80b8bdcbfc3359f0
Janetcueva22 <104587149+Janetcueva22@users.noreply.github.com>
2022-08-18 23:57:57 -0500

    Add files via upload

---
+++ src/test/java/com/idat/MiPrimerEurekaServer/MiPrimerEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.idat.MiPrimerEurekaServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiPrimerEurekaServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
